729cba501eccd55e227aafaf7b4d583ad7164c81
Mohammad Jayeed <thisisjayeed@gmail.com>
2022-07-12 15:35:40 +0600

    updated

--- airquality/settings.py
+++ airquality/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0jk$2^=ue2ah5&na+#^tabo&2#_2akt1_wna6#qfj5r$1jz0y!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
